a2cef0dc468c7533251c2037236a517c9acce2bb
nfukasawa <get.it.on.fuka@gmail.com>
2017-05-07 11:12:31 +0900

    Add ex.1.4.

---
+++ ch.01/ex.04/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Dup struct {
+	Count int
+	Files []string
+}
+
+type DupMap map[string]*Dup
+
+func NewDupMap() DupMap {
+	return DupMap{}
+}
+
+func (d DupMap) CountFiles(files []string) error {
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		d.Count(f)
+	}
+	return nil
+}
+
+func (d DupMap) Count(f *os.File) error {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		txt := input.Text()
+		if dup, ok := d[txt]; ok {
+			dup.Files = append(dup.Files, f.Name())
+			dup.Count++
+		} else {
+			d[txt] = &Dup{
+				Files: []string{f.Name()},
+				Count: 1,
+			}
+		}
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	return nil
+}
+func (d DupMap) EnumDup() map[string]*Dup {
+	dups := map[string]*Dup{}
+	for txt, dup := range d {
+		if dup.Count > 1 {
+			dups[txt] = dup
+		}
+	}
+	return dups
+}
+
+func main() {
+	dupmap := NewDupMap()
+	files := os.Args[1:]
+
+	var err error
+	if len(files) == 0 {
+		err = dupmap.Count(os.Stdin)
+	} else {
+		err = dupmap.CountFiles(files)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "dup: %v\n", err)
+	}
+
+	for txt, dup := range dupmap.EnumDup() {
+		fmt.Printf("%d\t%s\t%q\n", dup.Count, txt, dup.Files)
+	}
+}
---
+++ ch.01/ex.04/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+import "reflect"
+
+func TestDupMap(t *testing.T) {
+	dupmap := NewDupMap()
+	dupmap.CountFiles([]string{"fixture/test1.txt", "fixture/test2.txt", "fixture/test3.txt"})
+
+	dups := dupmap.EnumDup()
+	if l := len(dups); l != 4 {
+		t.Fatalf("len(dupmap.EnumDup()) => %d, want: 4", l)
+	}
+
+	testDup(t, dups, "hello", 2, "fixture/test1.txt", "fixture/test2.txt")
+	testDup(t, dups, "bbb", 2, "fixture/test1.txt", "fixture/test2.txt")
+	testDup(t, dups, "fuga", 3, "fixture/test1.txt", "fixture/test1.txt", "fixture/test3.txt")
+	testDup(t, dups, "foo", 2, "fixture/test2.txt", "fixture/test3.txt")
+}
+
+func testDup(t *testing.T, dups map[string]*Dup, txt string, count int, files ...string) {
+	dup, ok := dups[txt]
+	if !ok {
+		t.Fatalf("dups[%s] is nil", txt)
+	}
+
+	if dup.Count != count {
+		t.Fatalf("dups[%s] count => %d, want: %d", dup.Count, count)
+	}
+
+	if !reflect.DeepEqual(dup.Files, files) {
+		t.Fatalf("dups[%s] files => %q, want: %q", dup.Files, files)
+	}
+}
